37fff2e844565cc16cf2cf3bbe2bacb71c56c3b1
charlesebastian <charlessebastian94@gmail.com>
2021-09-06 16:23:29 +0530

    SchemaRegistry

--- src/main/java/com/kafka/springbootproducer/controller/KafkaProducer.java
+++ src/main/java/com/kafka/springbootproducer/controller/KafkaProducer.java
@@ -1,24 +1,28 @@
 package com.kafka.springbootproducer.controller;
 
-import lombok.RequiredArgsConstructor;
+import com.kafka.springbootproducer.Customers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kafka.springbootproducer.entity.Customer;
-
-@RequiredArgsConstructor
 @Component
 @RestController
 public class KafkaProducer {
 
-	private final KafkaTemplate<String, Customer> template;
+	private final KafkaTemplate<String, Customers> template;
+	@Autowired
+	public KafkaProducer(KafkaTemplate<String, Customers> kafkaTemplate) {
+		this.template = kafkaTemplate;
+	}
 
 	@PostMapping("/addCustomer")
-	public String addCustomer(@RequestBody Customer customer) {
+	public String addCustomer(@RequestBody Customers customer) {
 		template.send("CustomerTopic",customer);
 		return "Producer has published the topic";
+
 	}
+
 }
